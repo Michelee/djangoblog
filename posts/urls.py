4fb7ea5674b26730a77883d84b96b57d2049aaea
Michele Ancheta <michelee.ancheta@gmail.com>
2017-05-18 23:28:31 -0400

    Create and update posts

--- posts/urls.py
+++ posts/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.post_home, name="home"),
-    url(r'^create/$', views.post_create),
+    url(r'^create/$', views.post_create, name="create"),
     url(r'^(?P<id>\d+)/$', views.post_detail, name="detail"),
-    url(r'^update/$', views.post_update),
+    url(r'^(?P<id>\d+)/update/$', views.post_update, name="update"),
     url(r'^delete/$', views.post_delete),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,16 +1,9 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from .models import Post
+from .forms import PostForm
 
 def post_home(request):
-	# if request.user.is_authenticated():
-	# 	context = {
-	# 		"title": "My Posts List"
-	# 	}
-	# else:
-	# 	context = {
-	# 		"title": "Posts List"
-	# 	}
 	queryset = Post.objects.all()
 	context = {
  		"title": "Posts List",
@@ -20,18 +13,41 @@ def post_home(request):
 	return render(request, "index.html", context)
 
 def post_create(request):
-	return HttpResponse("<h1>Create</h1>")
+	form = PostForm(request.POST or None)
+	if form.is_valid():
+		instance = form.save(commit=False)
+		instance.save()
+		return HttpResponseRedirect(instance.get_absolute_url())
 
-def post_detail(request, id):
+	context = {
+		"title": "Create New Note",
+		"form": form
+	}
+	return render(request, "post_form.html", context)
+
+def post_detail(request, id=None):
 	instance = get_object_or_404(Post, id=id)
 	context = {
 		"title": instance.title,
 		"instance": instance
 	}
-	return render(request, "detail.html", context)
+	return render(request, "post_detail.html", context)
 
-def post_update(request):
-	return HttpResponse("<h1>Update</h1>")
+def post_update(request, id=None):
+	instance = get_object_or_404(Post, id=id)
+	form = PostForm(request.POST or None, instance=instance)
+	if form.is_valid():
+		instance = form.save(commit=False)
+		instance.save()
+		return HttpResponseRedirect(instance.get_absolute_url())
+
+	context = {
+		"title": instance.title,
+		"instance": instance,
+		"form": form
+	}
+	return render(request, "post_form.html", context)
+
 
 def post_delete(request):
 	return HttpResponse("<h1>Delete</h1>")
